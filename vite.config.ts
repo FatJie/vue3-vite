d613c448063018360d2d8c06545aa20f67d1564d
fatjie <372761781@qq.com>
2022-06-08 18:23:03 +0800

    npm install vite-plugin-eslint, 利用此依赖在开发和构建项目时进行eslint校验

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    eslintPlugin({
+      // 配置选项
+      cache: false, // 禁用eslint缓存
+      fix: true // 代码保存时执行 eslint 校验
+    })
+  ]
 })
